4f151e449123817ad4d15d5179a35b9909af1ecf
Daniel Gadd <dangad999@gmail.com>
2014-06-17 09:33:21 +1200

    Tidy up i3 project folder
    Removed old configuration files and script from i3 folder

--- i3/autostart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#Lock screen after 7mins of inactivity
-xautolock -time 7 -locker 'i3lock -c000000' &
--- i3/i3bar/conky.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "{\"version\":1}"
-echo "[[] ,"
-conky -c ~/.shell/i3/i3bar/conkyrc
