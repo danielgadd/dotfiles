70230596e4a12bf04fc67198ee1f5efe9a97542e
Daniel Gadd <dangad999@gmail.com>
2014-07-16 09:17:07 +1200

    Added Ignore all dups in history to clean up reverse I search

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -9,6 +9,7 @@ setopt append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_all_dups # ignore all dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt inc_append_history
