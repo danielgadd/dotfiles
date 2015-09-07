8f59d5ed636722b22aa3aa0ee92674f136c51401
Daniel Gadd <dangad999@gmail.com>
2015-09-07 15:44:25 +1200

    Added Irssi alias to suppport 256-term emulation

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -40,3 +40,5 @@ alias afind='ack-grep -il'
 alias ports='echo " " && echo -e "\033[1mListening Ports: ipv4\033[m" && netstat -tuan4 | grep LISTEN && echo " " && echo -e "\033[1mListening Ports: ipv6\033[m" && netstat -tuan6 | grep LISTEN'
 
 alias ssh="rm ~/.ssh/config; cat ~/.ssh/conf.d/* | sed '/vim: syntax/d' > ~/.ssh/config && chmod 0600 ~/.ssh/config && ssh"
+
+alias irssi="TERM=screen-256color irssi"
