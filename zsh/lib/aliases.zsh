3d37036fc3f99188016965b3754e864493c5c982
Daniel Gadd <dangad999@gmail.com>
2015-12-01 15:12:17 +1300

    Added tmux alias for zsh

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -38,7 +38,8 @@ alias afind='ack-grep -il'
 # My Aliases used for commands I like to use:
 
 alias ports='echo " " && echo -e "\033[1mListening Ports: ipv4\033[m" && netstat -tuan4 | grep LISTEN && echo " " && echo -e "\033[1mListening Ports: ipv6\033[m" && netstat -tuan6 | grep LISTEN'
-
 alias ssh="rm ~/.ssh/config; cat ~/.ssh/conf.d/* | sed '/vim: syntax/d' > ~/.ssh/config && chmod 0600 ~/.ssh/config && ssh"
-
 alias irssi="TERM=screen-256color irssi"
+
+# Set an alias for tmux (as it has problem with 256 colours)
+alias tmux="tmux -2"
