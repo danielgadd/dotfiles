7bc848004c6c49671f2e740067c5bfeb1d3bd69a
Daniel Gadd <daniel@redshield.co>
2019-09-16 09:08:20 +1200

    Rebuilt dotiles repo
    Rebuilt my dofiles using better plugin management for my major
    applications and shell, making a cleaner/faster shell

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Installer Script to install the zsh shell customisations.
+#
+#This is a run once installer as updates are pushed by either git _OR_ rsync pushing changes up
+
+# What does this do:
+# 1. check for zsh shell program - will error and exit if doesnt exsist
+# 2. create symlink for zshrc to ~/.zshrc
+# 3. Install .vim and ~/.vimrc
+# 3. create INSTALLED.file to stop the installation
+
+
+if [ -f /bin/zsh ]
+then
+  echo "zsh found - setup can continue."
+else
+  echo "zsh shell could not be found. Exiting to bash."
+  exit 0
+fi
+# Keep it simple
+
+ # Setup ZSH and zplugins
+ ln -s ~/.shell/zsh/zshrc ~/.zshrc
+ git clone https://github.com/zdharma/zplugin.git ~/.shell/zsh/zplugin/bin
+
+ # Setup Vim and Vundle
+ ln -s ~/.shell/vim/vimrc ~/.vimrc
+ git clone https://github.com/VundleVim/Vundle.vim.git ~/.shell/vim/bundle/Vundle.vim
+ vim +PluginInstall +qall
+ exit 0
---
+++ vim/plugins.vim
@@ -0,0 +1,30 @@
+set rtp+=~/.shell/vim/bundle/Vundle.vim
+
+call vundle#begin('~/.shell/vim/bundle')
+
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'airblade/vim-gitgutter'
+Plugin 'bronson/vim-trailing-whitespace'
+Plugin 'easymotion/vim-easymotion'
+Plugin 'editorconfig/editorconfig-vim'
+Plugin 'itchyny/lightline.vim'
+Plugin 'itchyny/vim-gitbranch'
+Plugin 'junegunn/fzf'
+Plugin 'junegunn/fzf.vim'
+Plugin 'junegunn/goyo.vim'
+Plugin 'morhetz/gruvbox'
+Plugin 'reedes/vim-colors-pencil'
+Plugin 'scrooloose/nerdtree'
+Plugin 'terryma/vim-multiple-cursors'
+Plugin 'tpope/vim-eunuch'
+Plugin 'tpope/vim-surround'
+Plugin 'jamessan/vim-gnupg'
+Plugin 'tpope/vim-fugitive'
+Plugin 'rodjek/vim-puppet'
+Plugin 'airblade/vim-rooter'
+Plugin 'takac/vim-hardtime'
+Plugin 'hashivim/vim-terraform'
+
+call vundle#end()
+
+filetype plugin indent on
