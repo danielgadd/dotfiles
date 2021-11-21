315f0b92dd4ada0ea1a9f1f9d12d1735fa10a62c
Daniel Gadd <git@gadd.co.nz>
2021-11-22 08:38:09 +1300

    Update git alias to figure what the main branch is

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -21,3 +21,16 @@ function open_command() {
 
   ${=open_cmd} "$@" &>/dev/null
 }
+
+# Check if main exists and use instead of master
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return
+    fi
+  done
+  echo master
+}
